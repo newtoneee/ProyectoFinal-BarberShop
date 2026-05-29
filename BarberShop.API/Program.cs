cc1b28500ec04fda84efdc3563731bbde069a4cd
isaac <armandoipz@hotmail.com>
2026-05-29 06:18:27 -0700

    feat(api): Configuración de políticas CORS y habilitación de servicios Web API locales

--- BarberShop.API/Program.cs
+++ BarberShop.API/Program.cs
@@ -4,7 +4,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Servicios
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+    
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
